dc07756cf416a592f038d768776dd8519661e49d
Jonas Fortes <jonascforte@hotmail.com>
2025-08-14 18:27:28 -0300

    test: adjust imports

--- src/place/place.controller.spec.ts
+++ src/place/place.controller.spec.ts
@@ -1,47 +1,65 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { PlaceController } from "./place.controller";
-import { PlaceService } from "./place.service";
-import { CloudinaryService } from "./cloudinary.service";
+import { Test, TestingModule } from '@nestjs/testing';
+import { PlaceController } from './place.controller';
+import { PlaceService } from './place.service';
+import { CloudinaryService } from './cloudinary.service';
+import { Place, placeType } from '@prisma/client';
+
+
+describe('PlaceController testes', () => {
+  let controller: PlaceController;
+  let placeService: jest.Mocked<PlaceService>;
+  let cloudinaryService: jest.Mocked<CloudinaryService>;
+
+  beforeEach(async () => {
+    const mockPlaceService = {
+      findAll: jest.fn(),
+      findPaginated: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+      remove: jest.fn(),
+    } as any;
+
+    const mockCloudinaryService = {
+      uploadImage: jest.fn(),
+      deleteImage: jest.fn(),
+    } as any;
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PlaceController],
+      providers: [
+        { provide: PlaceService, useValue: mockPlaceService },
+        { provide: CloudinaryService, useValue: mockCloudinaryService },
+      ],
+    }).compile();
+
+    controller = module.get<PlaceController>(PlaceController);
+    placeService = module.get(PlaceService);
+    cloudinaryService = module.get(CloudinaryService);
+  });
+
+  // "Deve listar todos os locais"
+  it('deve listar todos os locais', async () => {
+    const places: Place[] = [
+      {
+        id: "1",
+        name: 'Bar Tunico',
+        type: placeType.BAR,
+        phone: '899223',
+        latitude: 23.6,
+        longitude: 23.5,
+        images: [],
+        created_at: new Date()
+      },
+    ];
+
+    placeService.findAll.mockResolvedValue(places);
+
+    expect(await controller.findAll()).toEqual(places);
 
 
 
-describe("PlaceController testes", ()=> {
+  });
 
-    let controller: PlaceController;
-    let placeService: jest.Mocked<PlaceService>;
-    let cloudinaryService: jest.Mocked<CloudinaryService>;
-
-    beforeEach(async () => {
-        const mockPlaceService = {
-            findAll: jest.fn(),
-            findPaginated: jest.fn(),
-            create: jest.fn(),
-            update: jest.fn(),
-            remove: jest.fn(),
-        } as any;
-
-        const mockCloudinaryService = {
-            uploadImage: jest.fn(),
-            deleteImage: jest.fn(),
-        } as any;
-
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [PlaceController],
-            providers: [
-                {provide: PlaceService, useValue: mockPlaceService},
-                {provide: CloudinaryService, useValue: mockCloudinaryService}
-            ]
-        }).compile()
-
-        controller = module.get<PlaceController>(PlaceController);
-        placeService = module.get(mockPlaceService);
-        cloudinaryService = module.get(mockCloudinaryService);
-    });
-
-    // "Deve listar todos os locais"
-    
-
-
-
-
-})
+  // "Deve listar locais paginados"
+  //...
+});
